7dc2dc807203bb9f7f385f7ff495b6acb532341c
Elham Aryanpur <elhamaryanpur5@gmail.com>
2020-04-11 22:39:10 +0430

    set minimum size

--- src/Frames.svelte
+++ src/Frames.svelte
@@ -4,15 +4,17 @@
 </script>
 
 <style>
-    iframe{
+    div{
         margin: 10px;
     }
 </style>
 
-<widgetbot
-  server={link}
-  channel={channel}
-  width="800"
-  height="600"
-  shard="https://e.widgetbot.io"
-></widgetbot>
+<div>
+    <widgetbot
+    server={link}
+    channel={channel}
+    width="800"
+    height="600"
+    shard="https://e.widgetbot.io"
+    ></widgetbot>
+</div>
